20e24340068e029f2ffed1bd7780c37b025d7450
Seyayaya <zj48@kent.ac.uk>
2024-10-24 16:39:45 +0000

    task 3

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,1 +1,11 @@
 ### Part Three -- your code goes here. 
+
+myList = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+orderedList = sorted(myList)
+newList = []
+for x in orderedList:
+    if x != 1:
+        newList.append(x)
+newList.append(7)
+newList.append(8)
+print(newList)
