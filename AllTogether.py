b4b2d0d0e1669fe1e8ecbbb7a286fc45af0d08c1
Seyayaya <zj48@kent.ac.uk>
2024-10-24 17:15:01 +0000

    wee

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,22 @@
 ### Part Four -- your code goes here. 
+import random
+
+myList = []
+for x in range(1,11):
+    myList.append(random.randint(1,100))
+
+print("Here are your numbers:")
+for x in myList:
+    print(x)
+
+i = 0
+while i < len(myList):
+    if myList[i] % 2 == 0:
+        myList.pop(i)
+    else:
+        i += 1
+    
+
+print(myList)
+
+
