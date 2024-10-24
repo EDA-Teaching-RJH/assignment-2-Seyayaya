8a4a10e9fb65b1831cfcd2adfccd867b4fe38d9e
Seyayaya <zj48@kent.ac.uk>
2024-10-24 16:20:04 +0000

    assignments 1 and 2 complete

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,11 @@
 ### Part Two -- your code goes here. 
+import random
+
+#random.int()
+answer = random.randint(1,100)
+guess = int(input("Please guess a number between 1-100\n"))
+
+while guess != answer:
+    guess = int(input("Sorry, That's the wrong answer, please try again\n"))
+print("That's Correct. Well done!")
+    
--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,6 @@
 ### Part One -- your code goes here. 
+
+for i in range(1,11):
+    i = i ** 2
+    print(i)
+
